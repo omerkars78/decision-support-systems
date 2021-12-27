36d06e96d9967f8f4e3987811c6ccc9ff58b02df
omerkars78 <81978907+omerkars78@users.noreply.github.com>
2021-12-27 20:58:28 +0300

    e
    e

--- php to html/anasayfa.php
+++ php to html/anasayfa.php
@@ -1,6 +1,10 @@
 <?php
 error_reporting(0);
 ?>
+<?php
+
+include("../php/baglanti.php");
+?>
 <!DOCTYPE html>
 <html lang="tr">
 
@@ -10,6 +14,7 @@ error_reporting(0);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="../css/style.css">
     <link rel="stylesheet" type="text/css" href="../css/reset.css">
+    <link rel="stylesheet" type="text/css" href="../css/anasayfa.css">
     <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/3.6.2/chart.min.js"></script>
     <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
@@ -27,18 +32,18 @@ error_reporting(0);
             </div>
 
             <button class="anasayfa">
-  
+
                 <a href="anasayfa.php"><i class="fas fa-home"> </i> Anasayfa</a>
             </button>
             <button class="siparis">
-              
+
                 <a href="kds_siparis.php"><i class="fas fa-shopping-cart"> </i> Sipariş</a>
             </button>
             <button class="kesimhane">
                 <a href="kds_kesimhane.php"><i class="fas fa-cut"> </i> Kesimhane</a>
             </button>
             <button class="makine_dikim">
-                
+
                 <a href="kds_makine_dikim.php"><i class="fas fa-tshirt"> </i> Makine Dikim</a>
             </button>
             <button class="kalite_kontrol">
@@ -54,131 +59,202 @@ error_reporting(0);
         </div>
 
         <div class="content_container">
-            
+
             <div class="ust_bar">
 
             </div>
-            
+
             <div class="gunluk_bilgi_kutusu">
                 <div class="gunluk_bilgiler">
-                    
+
                     <div id="tarih">
 
-                    
+
                     </div>
 
                     <div id="saat">
 
-                    
+
                     </div>
-                   
+
                     <div id="kesimdeki_urun">
 
-                    
+
                     </div>
 
                     <div id="bekleyen_siparis">
 
-                    
+
                     </div>
 
                 </div>
             </div>
-            
-            <div class="genel_bilgi_container">
-                
-                <div id="first_box" >
-                <p>Sipariş İd leri ve Sipariş Adetleri</p>
-                    <div class="box-2">
-                        <div class="activity-card">
 
-                            <div class="chart">
-                                <canvas id="myChart"></canvas>
-                            </div>
-                            <?php
-                            $siparisler = [];
-                            $sql = "SELECT siparis.siparis_id, siparis.siparis_adeti
+            <div class="genel_bilgi_container">
+
+                <div class="first_box">
+                    <p>Sipariş İd leri ve Sipariş Adetleri</p>
+
+                    <div class="box-2">
+                    <div class="activity-card">
+
+                        <div class="chart">
+                            <canvas id="myChart"></canvas>
+                        </div>
+                        <?php
+                        $siparisler = [];
+                        $sql = "SELECT siparis.siparis_id, siparis.siparis_adeti
                   FROM siparis 
                   ;";
-                            $result = $con->query($sql);
+                        $result = $con->query($sql);
 
-                            if ($result->num_rows > 0) {
-                                // output data of each row
-                                while ($row = $result->fetch_assoc()) {
-                                    $siparisler[$row["siparis_id"]] += $row["siparis_adeti"];
-                                }
-                            } else {
-                                echo "0 results";
+                        if ($result->num_rows > 0) {
+                            // output data of each row
+                            while ($row = $result->fetch_assoc()) {
+                                $siparisler[$row["siparis_id"]] += $row["siparis_adeti"];
                             }
-                            $con->close();
+                        } else {
+                            echo "0 results";
+                        }
+                        $con->close();
 
 
-                            ?>
-                            <script>
-                                const data = {
-                                    labels: [
+                        ?>
+                        <script>
+                            const data = {
+                                labels: [
+                                    <?php
+                                    foreach ($siparisler as $key => $value) {
+                                        echo "'" . $key . "',";
+                                    }
+                                    ?>
+                                ],
+                                datasets: [{
+                                    label: 'My First Dataset',
+                                    data: [
                                         <?php
                                         foreach ($siparisler as $key => $value) {
-                                            echo "'" . $key . "',";
+                                            echo $value . ",";
+                                        }
+                                        ?>
+                                    
+                                    ],
+                                    backgroundColor: [
+                                        'rgb(255, 99, 132)',
+                                        'rgb(54, 162, 235)',
+                                        'rgb(255, 205, 21)',
+                                        'rgb(210, 0, 120)',
+                                        'rgb(210, 98, 120)',
+                                    ],
+                                    hoverOffset: 4
+                                }]
+                            };
+
+                            const config = {
+                                type: 'pie',
+                                data: data,
+                            };
+
+                            
+                            const ctx = document.getElementById('myChart');
+                            const myChart = new Chart(ctx, config);
+                        </script>
+                            
+                    </div>
+                </div>
+
+
+            </div>
+
+            <div class="second_box">
+            <div class="box-2">
+                    <div class="activity-card">
+
+                        <div class="chart">
+                            <canvas id="myChart"></canvas>
+                        </div>
+                        <?php
+                        $siparisler = [];
+                        $sql = "SELECT siparis.siparis_id, ((kesimhane.cikan_parca)-(siparis.istenen_parca)) as kesim_farki
+                        FROM siparis , kesimhane 
+                        WHERE siparis.siparis_id = kesimhane.siparis_id;
+                        ";
+                        
+                     
+                        $result = $con->query($sql);
+
+                        if ($result->num_rows > 0) {
+                            // output data of each row
+                            while ($row = $result->fetch_assoc()) {
+                                $kesim_farki[$row["siparis_id"]] += $row["kesim_farki"];
+                            }
+                        } else {
+                            echo "0 results";
+                        }
+                        $con->close();
+
+
+                        ?>
+                        <script>
+                            const data = {
+                                labels: [
+                                    <?php
+                                    foreach ($kesim_farki as $key => $value) {
+                                        echo "'" . $key . "',";
+                                    }
+                                    ?>
+                                ],
+                                datasets: [{
+                                    label: 'Kesim Farkları',
+                                    data: [
+                                        <?php
+                                        foreach ($kesim_farki as $key => $value) {
+                                            echo $value . ",";
                                         }
                                         ?>
                                     ],
-                                    datasets: [{
-                                        label: 'My First Dataset',
-                                        data: [
-                                            <?php
-                                            foreach ($siparisler as $key => $value) {
-                                                echo $value . ",";
-                                            }
-                                            ?>
-                                        ],
-                                        backgroundColor: [
-                                            'rgb(255, 99, 132)',
-                                            'rgb(54, 162, 235)',
-                                            'rgb(255, 205, 21)',
-                                            'rgb(210, 0, 120)',
-                                            'rgb(210, 98, 120)',
-                                        ],
-                                        hoverOffset: 4
-                                    }]
-                                };
+                                    backgroundColor: [
+                                        'rgb(255, 99, 132)',
+                                        'rgb(54, 162, 235)',
+                                        'rgb(255, 205, 21)',
+                                        'rgb(210, 0, 120)',
+                                        'rgb(210, 98, 120)',
+                                    ],
+                                    hoverOffset: 4
+                                }]
+                            };
 
-                                const config = {
-                                    type: 'pie',
-                                    data: data,
-                                };
+                            const config = {
+                                type: 'line',
+                                data: data,
+                            };
 
 
-                                const ctx = document.getElementById('myChart');
-                                const myChart = new Chart(ctx, config);
-                            </script>
+                            const ctx = document.getElementById('myChart');
+                            const myChart = new Chart(ctx, config);
+                        </script>
 
-                        </div>
                     </div>
-                
                 </div>
 
-                <div id="second_box">
-
-
-                </div>
-
-                <div id="thirth_box">
-                    
-                </div>
-            </div>
-        
-        <div class="genel_bilgi_container_2">  
-            <div id="fourth_box">
-                
             </div>
 
-            <div id="fifth_box">
-                
-            </div>
-        </div>  
+            <div class="thirth_box">
 
+            </div>
         </div>
+
+        <div class="genel_bilgi_container_2">
+            <div class="fourth_box">
+
+            </div>
+
+            <div class="fifth_box">
+
+            </div>
+        </div>
+
+    </div>
 
 
     </div>
